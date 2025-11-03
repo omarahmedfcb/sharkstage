48e3bbfd85ed7788cb467c4e033d794839b919a8
bahaa-mustafa <bahaamustafabadr@gmail.com>
2025-11-03 19:22:01 +0200

    Wrap children and Chatbot components in StoreProvider within RootLayout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Chatbot from "./components/Chatbot";
+import StoreProvider from "./StoreProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,8 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <Chatbot />
+        <StoreProvider>
+          {children}
+          <Chatbot />
+        </StoreProvider>
       </body>
     </html>
   );
