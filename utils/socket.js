253286463eb1fda6d8fcce40366c2b8da871d831
Omar Ahmed <manoomarfcb2000@gmail.com>
2025-11-18 01:57:39 +0200

    socket config 3

--- utils/socket.js
+++ utils/socket.js
@@ -1,8 +1,12 @@
 import { io } from "socket.io-client";
 
 const socket = io(process.env.NEXT_PUBLIC_API_URL, {
+  transports: ["websocket", "polling"],
   withCredentials: true,
-  transports: ["websocket"],
+  autoConnect: true,
+  reconnection: true,
+  reconnectionDelay: 1000,
+  reconnectionAttempts: 5,
 });
 
 export default socket;
