342c364a18cad17a60a507c108025d4d6fcf78b2
Omar Ahmed <manoomarfcb2000@gmail.com>
2025-11-18 01:49:32 +0200

    socket config 2

--- utils/socket.js
+++ utils/socket.js
@@ -3,7 +3,6 @@ import { io } from "socket.io-client";
 const socket = io(process.env.NEXT_PUBLIC_API_URL, {
   withCredentials: true,
   transports: ["websocket"],
-  auth: { token },
 });
 
 export default socket;
